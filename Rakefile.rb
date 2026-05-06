160b6b40dbaafe4c7411acbe153f4064dc483a6b
Josh Holtrop <jholtrop@gmail.com>
2026-05-06 08:40:24 -0400

    Fix gen_large_project generated project

--- Rakefile.rb
+++ Rakefile.rb
@@ -97,10 +97,8 @@ task :gen_large_project, [:size] => :build_dist do |task, args|
   end
   File.open("large_project/Rsconscript", "w") do |fh|
     fh.puts <<EOF
-default do
-  Environment.new do |env|
-    env.Program("project", glob("src/*.c"))
-  end
+env do |env|
+  env.Program("project", glob("src/*.c"))
 end
 EOF
   end
